e217b23ac765fc488f4f2515a44888bfeaf5ab3e
ahmedasad236 <ahmedasad236@gmail.com>
2024-04-24 00:48:58 +0000

    Setup mage container for orchestration

---
+++ mage/us-accidents/data_loaders/load_data_locally.py
@@ -0,0 +1,31 @@
+from mage_ai.io.file import FileIO
+import pandas as pd
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_file(*args, **kwargs):
+    """
+    Template for loading data from filesystem.
+    Load data from 1 file or multiple file directories.
+
+    For multiple directories, use the following:
+        FileIO().load(file_directories=['dir_1', 'dir_2'])
+
+    Docs: https://docs.mage.ai/design/data-loading#fileio
+    """
+    filepath = '/home/src/US_Accidents_March23.csv'
+    df = pd.read_csv(filepath)
+    print(df.head(2))
+    return df
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
