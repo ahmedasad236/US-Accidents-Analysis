c675a8efb089a2fe952549eb3d5d9c1a59044bda
ahmedasad236 <ahmedasad236@gmail.com>
2024-05-07 11:36:16 +0000

    Unseccessful try to convert string data to one hot vector

--- spark/linear_regression_mr.py
+++ spark/linear_regression_mr.py
@@ -1,20 +1,8 @@
-from pyspark.sql import SparkSession
 import numpy as np
 import random
+from pyspark.sql.functions import size
 
-spark = SparkSession.builder \
-    .master("local[8]") \
-    .appName("US Accidents") \
-    .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
-    .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile",key_filepath  ) \
-    .config("spark.hadoop.google.cloud.auth.service.account.enable", "true") \
-    .getOrCreate()
-
-spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
-
-
-
-def linear_regression(data, learning_rate, num_iterations, num_features):
+def linear_regression(data, learning_rate, num_iterations):
   """
   Performs linear regression using MapReduce and Gradient Descent for N features
 
@@ -22,21 +10,21 @@ def linear_regression(data, learning_rate, num_iterations, num_features):
       data: RDD containing data points with features and target variable
       learning_rate: Learning rate for gradient descent
       num_iterations: Number of iterations for gradient descent
-      num_features: Number of features in the data
 
   Returns:
       A tuple containing the final weights and the cost history
   """
+  num_features = data.select(size(data["features"])).first()[0]
   # Initialize weights with zeros
   weights = [random.random() for _ in range(num_features)]
 
   cost_history = []
   for i in range(num_iterations):
     # Map step: Calculate gradient for each data point
-    gradient_rdd = data.map(lambda point: calculate_gradient(point, weights, num_features))
+    gradient_df = data.map(lambda point: calculate_gradient(point, weights, num_features))
 
     # Reduce step: Aggregate gradients to get overall update
-    total_gradient = gradient_rdd.reduce(lambda g1, g2: np.add(g1, g2))
+    total_gradient = gradient_df.reduce(lambda g1, g2: np.add(g1, g2))
 
     # Update weights using learning rate
     # w = w - lr * gradient
@@ -50,14 +38,13 @@ def linear_regression(data, learning_rate, num_iterations, num_features):
 
     return weights, cost_history
 
-  def calculate_gradient(point, weights, num_features):
+  def calculate_gradient(point, weights):
     """
     Calculates the gradient for a single data point
   
     Args:
         point: A tuple containing features and target variable [ex : ([1, 2,4,5,6], 1)]
         weights: Current weights for the model
-        num_features: Number of features in the data
   
     Returns:
         A list containing gradients for each weight
@@ -98,19 +85,17 @@ def predict(point, weights):
   return np.dot(weights, features)
 
 # Example usage
-spark = SparkSession.builder.appName("LinearRegression").getOrCreate()
 
 # Assuming your data is loaded into an RDD named 'data'
 # Each element in 'data' should be a tuple of (features, target)
 
 #hyperparameters
-learning_rate = 0.0001
-num_iterations = 100
+# learning_rate = 0.0001
+# num_iterations = 100
 
 
-weights, cost_history = linear_regression(data, learning_rate=learning_rate, num_iterations=num_iterations, num_features=20)
+# weights, cost_history = linear_regression(data, learning_rate=learning_rate, num_iterations=num_iterations, num_features=20)
 
-print("Final weights:", weights)
-print("Cost history:", cost_history)
+# print("Final weights:", weights)
+# print("Cost history:", cost_history)
 
-spark.stop()
